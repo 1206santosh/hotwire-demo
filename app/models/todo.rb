afa655923a32663253dad7e5e962116f1ee0562a
Santosh Ghode <santoshghode@pop-os.localdomain>
2021-06-09 14:41:04 +0530

    some changes

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,5 @@
 class Todo < ApplicationRecord
     # validates :body
-
-    after_create_commit {broadcast_append_to 'todos'}
+    after_create_commit {broadcast_append_to 'new_todos'}
+    after_update_commit {broadcast_replace_to 'new_todos'}
 end 
